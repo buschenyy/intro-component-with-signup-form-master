5b875c2dcdc0817cab4f959ae51f1505c8a5a8fa
buschenyy <buschenyy@gmail.com>
2023-03-27 23:24:43 +0200

    responsive desktop

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -4,11 +4,25 @@ import { SubForm } from './SubForm'
 
 const Container = styled.div`
   box-sizing: border-box;
-  width: 375px;
+  min-width: 375px;
   background: url(${introBg}), hsl(0, 100%, 74%);
   font-family: Poppins;
   color: white;
   padding: 70px 30px 37px;
+  @media (min-width: 375px) {
+    max-width: 1440px;
+    min-height: 100vh;
+    padding-inline: 12.083vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto auto;
+    align-content: center;
+    align-items: center;
+    column-gap: 50px;
+  }
+  @media (max-width: 810px) {
+    padding: 70px 30px 37px;
+  }
 `
 
 const Title = styled.h2`
@@ -16,12 +30,36 @@ const Title = styled.h2`
   font-weight: 600;
   font-size: 27px;
   line-height: 36px;
+  @media (min-width: 375px) {
+    text-align: start;
+    font-weight: 700;
+    line-height: 52px;
+    font-size: 48px;
+    margin-bottom: 15px;
+    transition-delay: 1s;
+    grid-row-start: 2;
+    grid-row-end: 3;
+    grid-column-start: 1;
+    grid-column-end: 1;
+    align-self: end;
+  }
 `
 
 const Text = styled.p`
   font-weight: 400;
   margin-bottom: 63px;
   line-height: 26px;
+  @media (min-width: 375px) {
+    text-align: start;
+    font-size: 16px;
+    margin-bottom: 106px;
+    transition-delay: 1s;
+    grid-row-start: 3;
+    grid-row-end: 4;
+    grid-column-start: 1;
+    grid-column-end: 1;
+    align-self: start;
+  }
 `
 
 const Card = styled.div`
@@ -34,10 +72,24 @@ const Card = styled.div`
   > span {
     font-weight: 600;
   }
+  @media (min-width: 375px) {
+    grid-row-start: 1;
+    grid-row-end: 2;
+    grid-column-start: 2;
+    grid-column-end: 2;
+  }
 `
 const FormContainer = styled(Card)`
   background: white;
   padding: 24px;
+  @media (min-width: 375px) {
+    padding: 40px;
+    margin: 0;
+    grid-row-start: 2;
+    grid-row-end: 4;
+    grid-column-start: 2;
+    grid-column-end: 2;
+  }
 `
 
 export const Intro = () => {
@@ -50,8 +102,8 @@ export const Intro = () => {
         invaluable.
       </Text>
       <Card>
-        <span>Try it free 7 days</span> then
-        <br /> $20/mo. thereafter
+        <span>Try it free 7 days</span> then{' '}
+        {window.innerHeight <= 375 ? <br /> : ''}$20/mo. thereafter
       </Card>
       <FormContainer>
         <SubForm />
